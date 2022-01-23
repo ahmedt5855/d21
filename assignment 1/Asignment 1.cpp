a7a03af0cbfba386ced40db5826d213a9f9b5160
ahmed5855 <ahmeddtarek35@gmail.com>
2022-01-23 19:14:23 +0200

    t

---
+++ assignment 1/Asignment 1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int x;
+	printf("Enter the number \n");
+	scanf("%d",&x);
+	if(x%2==0)
+	printf("The number is odd = 0 \n The number is even = 1");
+	else ;
+	printf("The number is odd = 1 \n The number is even = 0");
+	return 0;
+}
---
+++ assignment 1/mo3ed 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int x;
+	printf("please enter the number \n");
+	scanf("%d",&x);
+	for(int i=0;i<=x;i++)
+	{
+		printf("%d \n",i*i);
+		
+	}
+	
+	return 0;
+}
